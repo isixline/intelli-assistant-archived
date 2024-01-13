dc889169edf81e1db5a876c1de103b922243db93
isixline <isixline@qq.com>
2024-01-13 14:24:27 +0800

    fix: get search_query from user_input

--- src/interactions/user_interaction.py
+++ src/interactions/user_interaction.py
@@ -31,7 +31,7 @@ def interaction_handle():
             print("Goodbye and have a nice day!")
             break
 
-        keyword, *search_query = user_input.lower().split(maxsplit=1)
+        keyword, search_query = user_input.lower().split(maxsplit=1)
 
         while not search_query:
             search_query = input("Please provide additional input after the keyword.")
