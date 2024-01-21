1ab55874f9f8cf3899863decf818db094bde4c38
isixline <isixline@qq.com>
2024-01-21 12:19:36 +0800

    test: use wikipedia_searcher_mock in test_wikipedia_searcher

--- tests/mocks/wikipedia_searcher_mock.py
+++ tests/mocks/wikipedia_searcher_mock.py
@@ -1,11 +1,11 @@
 import wikipedia
 
-def mock_wikipedia_summary(mock, query):
-        mock.return_value = query
+def mock_wikipedia_summary(mock, search):
+        mock.return_value = search
 
-def mock_wikipedia_disambiguation_error(mock, query):
-        mock.side_effect = wikipedia.exceptions.DisambiguationError(['Option1', 'Option2'], query)
+def mock_wikipedia_disambiguation_error(mock):
+        mock.side_effect = wikipedia.exceptions.DisambiguationError(['Option1', 'Option2'], 'Please provide a more specific search.')
 
-def mock_wikipedia_page_error(mock, query):
-        mock.side_effect = wikipedia.exceptions.PageError(query)
+def mock_wikipedia_page_error(mock):
+        mock.side_effect = wikipedia.exceptions.PageError('Sorry, no results found.')
 
--- tests/unit/searchers/test_wikipedia_searcher.py
+++ tests/unit/searchers/test_wikipedia_searcher.py
@@ -1,29 +1,32 @@
 import wikipedia
 from unittest.mock import patch
 from src.searchers.wikipedia_searcher import search_wikipedia
+from tests.mocks.wikipedia_searcher_mock import mock_wikipedia_summary, mock_wikipedia_disambiguation_error, mock_wikipedia_page_error
 
 @patch('src.searchers.wikipedia_searcher.wikipedia.summary')
 def test_search_wikipedia_summary_success(mock_summary):
     search = 'Python programming language'
-    mock_summary.return_value = 'Python is a high-level programming language.'
+    mock_wikipedia_summary(mock_summary, search)
 
     result = search_wikipedia(search=search)
 
-    assert result == 'Python is a high-level programming language.'
+    assert result == "Python programming language"
     mock_summary.assert_called_once_with(search, sentences=1)
 
-@patch('src.searchers.wikipedia_searcher.wikipedia.summary', side_effect=wikipedia.exceptions.DisambiguationError(['Option1', 'Option2'], 'test query'))
+@patch('src.searchers.wikipedia_searcher.wikipedia.summary')
 def test_search_wikipedia_disambiguation_error(mock_summary):
     search = 'Ambiguous search'
-    
+    mock_wikipedia_disambiguation_error(mock_summary)
+
     result = search_wikipedia(search=search)
 
     assert result == 'Please provide a more specific search.'
     mock_summary.assert_called_once_with(search, sentences=1)
 
-@patch('src.searchers.wikipedia_searcher.wikipedia.summary', side_effect=wikipedia.exceptions.PageError('test'))
+@patch('src.searchers.wikipedia_searcher.wikipedia.summary')
 def test_search_wikipedia_page_error(mock_summary):
     search = 'Nonexistent Page'
+    mock_wikipedia_page_error(mock_summary)
     
     result = search_wikipedia(search=search)
 
